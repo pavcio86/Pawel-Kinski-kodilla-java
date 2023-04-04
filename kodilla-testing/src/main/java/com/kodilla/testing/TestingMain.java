5cd6e5cd3bd69f1ebd6997b5c7a6b075648a7e94
Pawel <pacvcio86developer@onet.pl>
2023-04-04 10:51:08 +0200

    6.2 pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+
+        Calculator simplecalculator = new Calculator();
+
+        int addResult = simplecalculator.add(6,4);
+        int substractResult = simplecalculator.substract(6,4);
+
+        if(addResult == 10){
+            System.out.println("addition test :   OK");
+        }else{
+            System.out.println("addition test : FAILED!");
+        }
+
+        if(substractResult == 2){
+            System.out.println("substraction test :   OK");
+        }else{
+            System.out.println("substraction test : FAILED!");
+        }
+
     }
 }
